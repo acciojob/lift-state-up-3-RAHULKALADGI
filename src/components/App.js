c4fda7ad64568ecee2e0a430ad763241fd3faa5f
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-07-04 17:24:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -10,16 +10,17 @@ const App= ()=> {
                 <h1>Parent Component</h1>
                 <div>
                     <h2>Child Component 1</h2>
-                    <button onClick={setOption("Option 1")}>Option 1</button>
+                    <button onClick={()=>setOption("Option 1")}>Option 1</button>
                 </div>
                 <div>
                     <h2>Child Component 2</h2>
-                    <button onClick={setOption("Option 2")}>Option 2</button>
+                    <button onClick={()=>setOption("Option 2")}>Option 2</button>
                 </div>
                 <p>Selected Option: {option}</p>
             </div>
         </div>
     )
 }
+
 
 export default App
